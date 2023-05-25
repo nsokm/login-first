f97be541467a91058675a1ec4662db4a095bfc4e
ns_okm <saetiewnw@gmail.com>
2023-05-25 17:12:15 +0700

    first

---
+++ login/lib/screen/home.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+
+class Homescreen extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("Register/login"),
+      ),
+    );
+  }
+}
