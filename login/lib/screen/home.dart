3e2229b104bfa3a77ffd11f7c80490d2e0a14119
ns_okm <saetiewnw@gmail.com>
2023-05-30 13:28:59 +0700

    add user

--- login/lib/screen/home.dart
+++ login/lib/screen/home.dart
@@ -6,6 +6,17 @@ class Homescreen extends StatelessWidget {
     return Scaffold(
       appBar: AppBar(
         title: Text("Register/login"),
+        body: Column(
+          children: [
+            SizedBox(
+              child: ElevatedButton.icon(
+                icon: Icon(Icons.add),
+                label: Text("สร้างบัญชีผู้ใช้", style: TextStyle(fontSize: 20)),
+                onPressed: () {},
+              ),
+            )
+          ],
+        ),
       ),
     );
   }
