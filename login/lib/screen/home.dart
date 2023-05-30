11463c6db684eaca924c897df0d61d154d830ba2
ns_okm <saetiewnw@gmail.com>
2023-05-30 13:39:07 +0700

    padding&width

--- login/lib/screen/home.dart
+++ login/lib/screen/home.dart
@@ -7,23 +7,28 @@ class Homescreen extends StatelessWidget {
       appBar: AppBar(
         title: Text("Register/login"),
       ),
-      body: Column(
-        children: [
-          SizedBox(
-            child: ElevatedButton.icon(
-              icon: Icon(Icons.add),
-              label: Text("สร้างบัญชีผู้ใช้", style: TextStyle(fontSize: 20)),
-              onPressed: () {},
+      body: Padding(
+        padding: const EdgeInsets.fromLTRB(10, 50, 10, 0),
+        child: Column(
+          children: [
+            SizedBox(
+              width: double.infinity,
+              child: ElevatedButton.icon(
+                icon: Icon(Icons.add),
+                label: Text("สร้างบัญชีผู้ใช้", style: TextStyle(fontSize: 20)),
+                onPressed: () {},
+              ),
             ),
-          ),
-          SizedBox(
-            child: ElevatedButton.icon(
-              icon: Icon(Icons.login),
-              label: Text("เข้าสู่ระบบ", style: TextStyle(fontSize: 20)),
-              onPressed: () {},
-            ),
-          )
-        ],
+            SizedBox(
+              width: double.infinity,
+              child: ElevatedButton.icon(
+                icon: Icon(Icons.login),
+                label: Text("เข้าสู่ระบบ", style: TextStyle(fontSize: 20)),
+                onPressed: () {},
+              ),
+            )
+          ],
+        ),
       ),
     );
   }
